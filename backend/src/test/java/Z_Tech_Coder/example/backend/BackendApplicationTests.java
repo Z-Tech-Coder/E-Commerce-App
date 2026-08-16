78a720f0a27a7c52a5057f283da8f52a00cfbfa3
Z-Tech-Coder <yousefelzoghpy3@gmail.com>
2026-08-16 03:44:19 +0300

    Initial project structure

---
+++ backend/src/test/java/Z_Tech_Coder/example/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Z_Tech_Coder.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
